db9883f90d7d0ae70df58d499e684e04319ab293
Michelle Pan <michelle-pan@users.noreply.github.com>
2020-01-11 11:11:16 -0800

    Add code to capture images

--- capture_image.py
+++ capture_image.py
@@ -6,6 +6,7 @@ import os
 # finds angle between robot's heading and the perpendicular to the targets
 class ImageCapture:
 
+<<<<<<< HEAD
     # initilaze variables
     def __init__(self, input, angle, distance):
 
@@ -81,3 +82,70 @@ class ImageCapture:
         print("Capture another image! yay")
         self.angle = input("Angle: ")
         self.distance = input("Distance: ")
+=======
+	# initilaze variables
+	def __init__(self, input, angle, distance):
+
+		self.input_path = input
+		self.angle = angle
+		self.distance = distance
+
+		try:
+			# if input is a camera port
+			self.input = cv2.VideoCapture(int(input))
+			self.set_camera_settings(input)
+		except:
+			# if input is a path
+			self.input = cv2.VideoCapture(input)
+
+		frame = self.get_frame()
+
+	def __enter__(self):
+		return self
+
+	def __exit__(self, type, value, tb):
+		self.input.release()
+		cv2.destroyAllWindows()
+		print("\nexited")
+
+	# sets exposure of the camera (will only work on Linux systems)
+	def set_camera_settings(self, camera_port):
+
+		camera_path = "/dev/video" + camera_port
+
+		try:
+			subprocess.call(["v4l2-ctl", "-d", camera_path, "-c", "exposure_auto=1"])
+			subprocess.call(["v4l2-ctl", "-d", camera_path, "-c", "exposure_absolute=1"])
+		except:
+			print("exposure adjustment not completed")
+
+	# returns a frame corresponding to the input type
+	def get_frame(self):
+
+		frame = None
+
+		# if input is a camera port, use VideoCapture()
+		if self.input_path.isdigit():
+			_, frame = self.input.read()
+		# if input is an image, use cv2.imread()
+		elif imghdr.what(self.input_path) is not None:
+			frame = cv2.imread(self.input_path)
+		# if input is a video, use VideoCapture()
+		else:
+			_, frame = self.input.read()
+
+		return frame
+
+	def update_frame(self):
+		frame = self.get_frame()
+		cv2.imshow("frame: " + str(self.input_path), frame)
+
+	def capture(self):
+		filename = "test_photos/" + self.angle + "degrees" + self.distance + "inches.png"
+		cv2.imwrite(filename, self.get_frame())
+
+	def prompt(self):
+		print("Capture another image! yay")
+		self.angle = input("Angle: ")
+		self.distance = input("Distance: ")
+>>>>>>> Add code to capture images
