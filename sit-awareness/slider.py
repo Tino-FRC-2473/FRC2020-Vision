6ec96773f046bf675eba7cd3eba258a165ff7c00
gargideshpande <gargid03@gmail.com>
2020-01-11 13:25:33 -0800

    Update slider.py

--- sit-awareness/slider.py
+++ sit-awareness/slider.py
@@ -14,15 +14,21 @@ slider.geometry("300x300")
 
 def set_y_slider():
     y_input = y_entry.get()
-    y.set(int(y_input))
+    # if y_input is None:
+    #     y_input = y.get()
+    y.set(float(y_input))
 
 def set_p_slider():
     p_input = p_entry.get()
-    p.set(int(p_input))
+    # if p_input is None:
+    #     p_input = p.get()
+    p.set(float(p_input))
 
 def set_r_slider():
     r_input = r_entry.get()
-    r.set(int(r_input))
+    # if r_input is None:
+    #     r_input = r.get()
+    r.set(float(r_input))
 
 def test_values():
     yaw = y.get()
@@ -39,18 +45,52 @@ def test_values():
     [0, 1, 0],
     [-math.sin(pitch), 0, math.cos(pitch)]
     ]) #y
-    roll_mat = np.matrix([
-    [1, 0, 0],
-    [0, math.cos(roll), -math.sin(roll)],
-    [0, math.sin(roll), math.cos(roll)]
-    ]) #x
+    roll_mat = np.matrix([[1, 0, 0],
+                          [0, math.cos(roll), -math.sin(roll)],
+                          [0, math.sin(roll), math.cos(roll)]
+                          ]) #x
 
     rot = yaw_mat * pitch_mat * roll_mat
+    # print(rot[2, 0])
+    # print(rot[2][0])
+    translated = cv2.Rodrigues(rot)[0]
+
+    # print("type:", type(cv2.Rodrigues(rot)))
+    # print(cv2.Rodrigues(rot))
+
     print(rot)
+    print(translated)
     # theta = math.acos(((rot[0, 0] + rot[1, 1] + rot[2, 2]) - 1) / 2)
     # multi = 1 / (2 * math.sin(theta))
     # print([yaw, pitch, roll])
     # use project points function in opencv
+    obj_points = [[ 0.0,  0.0, 0],
+                  [ 3.5,  5.5, 0],
+                  [-3.5,  5.5, 0],
+                  [-3.5, -5.5, 0],
+                  [ 3.5, -5.5, 0],
+                  [ 1.5,  3.5, 0],
+                  [-1.5,  3.5, 0],
+                  [-1.5, -3.5, 0],
+                  [ 1.5, -3.5, 0]]
+    # mx = rectangles[0].get_center().x
+    # my = rectangles[0].get_center().y
+    #
+    # img_points = []
+    # img_points.append([0,0])
+    #
+    # for r in rectangles:
+    #     for p in r.get_points():
+    #         img_points.append([p.x - mx, p.y - my])
+
+    obj_points = np.float64(obj_points)
+
+
+    camera_matrix = np.float64([[self.FOCAL_LENGTH_PIXELS, 0,                        self.SCREEN_WIDTH/2],
+                                [0,                        self.FOCAL_LENGTH_PIXELS, self.SCREEN_HEIGHT/2],
+                                [0,                        0,                        1]])
+
+    cv2.projectPoints(obj_points, translated, , camera_matrix, )
 
 
 
