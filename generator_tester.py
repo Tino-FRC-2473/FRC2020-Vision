62e08a3687ab890dd49ec27865e24b4f3895e951
Amogh Shivaram <30681603+amoghs1912@users.noreply.github.com>
2020-01-13 20:36:38 -0800

    added generator classes
    Added files with DepthDataGenerator (to generate a 2d array from a depth data CSV file and a 3d array for RGB data from an image) and DepthLiveGenerator (to generate an array with distance data read from the RealSense camera).

---
+++ generator_tester.py
@@ -0,0 +1,23 @@
+import argparse
+import numpy as np
+import time
+from depth_data_generator import DepthDataGenerator
+from depth_live_generator import DepthLiveGenerator
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("generator", help="which of the generator classes to test")
+parser.add_argument("inputDepth", help="path of the CSV file to read from")
+parser.add_argument("inputImage", help="path of the image file to read from")
+parser.add_argument("destination", help="file to save the results to")
+args = parser.parse_args()
+
+if args.generator == "DepthDataGenerator":
+    depth_data_generator = DepthDataGenerator(args.inputDepth, args.inputImage)
+    arr, _ = depth_data_generator.generate()
+    np.savetxt(args.destination, arr, delimiter=",", fmt="%s")
+elif args.generator == "DepthLiveGenerator":
+    depth_live_generator = DepthLiveGenerator(1)
+    time.sleep(5)
+    arr = depth_live_generator.generate()
+    np.savetxt(args.destination, arr, delimiter=",", fmt="%s")
