2a3968eb615f21b8eefc19b17b94b2a1cc0d9ca5
Siddarth Mamidanna <siddarthm272@gmail.com>
2020-01-14 20:41:36 -0800

    Rename all files to naming convention
    Change parameters of run detection method in the detectors, making the
    methods no longer take in a calibrator - calibrator is now built in to
    the detector classes themselves

--- loading_bay_detector.py
+++ loading_bay_detector.py
@@ -1,11 +1,12 @@
 import cv2
 import numpy as np
-from cv_color_calibration import GreenCalibration
+from green_calibration import GreenCalibration
 class LoadingBayDetector:
 
     # Constructor
     # int input_type(0-image, 1-video, 2-live-camera, 3-depth-image, 4-depth-video)
     def __init__(self, input_img): 
+        self.calibrator = GreenCalibration()
         inp = None
         if(input_type == 0):
             inp = 
@@ -21,19 +22,16 @@ class LoadingBayDetector:
     # Runs the detector, taking in the input image and calibrator object. This means that it'll actually
     # detect the loading bay and then it'll calibrate to the loading bay green color. This method returns 
     # the contours of the actual loading bay.
-    def run_detector(self, inp, calibrator):
+    def run_detector(self, inp):
         img = inp
         img = cv2.GaussianBlur(img, (3,3), cv2.BORDER_DEFAULT) #blurs image
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(hsv, calibrator.LOW_GREEN, calibrator.HIGH_GREEN)
+        mask = cv2.inRange(hsv, self.calibrator.LOW_GREEN, self.calibrator.HIGH_GREEN)
         contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         contours.sort(key=lambda c: cv2.contourArea(c), reverse=True)
         
         greens = hsv[np.where((mask == 255))]
 
-        calibrator.get_new_hsv(greens)
-
-        cv2.imshow("img", img)
-        cv2.imshow("mask", mask)
+        self.calibrator.get_new_hsv(greens)
 
         return contours
