f52a4727f18317425b7cc5215e29719411aaa48c
Amogh Shivaram <30681603+amoghs1912@users.noreply.github.com>
2020-01-14 19:02:57 -0800

    created class to work with two cameras
    Added a file with a class that takes input from two cameras.

---
+++ two_camera_tester.py
@@ -0,0 +1,18 @@
+import argparse
+import cv2
+from two_camera_setup import TwoCameraSetup
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input_first", help="read from the given camera port as the first camera", default="0")
+parser.add_argument("input_second", help="read from the given camera port as the second camera", default="1")
+args = parser.parse_args()
+
+setup = TwoCameraSetup(args.input_first, args.input_second)
+
+with setup:
+    while True:
+        setup.run_cv()
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
