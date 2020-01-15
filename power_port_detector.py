0df61862d8dd782ff4d631a071ca2b15d9e135ad
Siddarth Mamidanna <siddarthm272@gmail.com>
2020-01-14 20:41:36 -0800

    Rename all files to naming convention
    Change parameters of run detection method in the detectors, making the
    methods no longer take in a calibrator - calibrator is now built in to
    the detector classes themselves

--- power_port_detector.py
+++ power_port_detector.py
@@ -1,11 +1,11 @@
 import cv2
-from cv_color_calibration import GreenCalibration
+from green_calibration import GreenCalibration
 class PowerPortDetector:
 
     # Constructor
     # int input_type(0-image, 1-video, 2-live-camera, 3-depth-image, 4-depth-video)
     def __init__(self, input_type): 
-        calib = GreenCalibration()
+        self.calibrator = GreenCalibration()
         inp = None
         if(input_type == 0):
             inp = 
@@ -18,21 +18,20 @@ class PowerPortDetector:
         else:
             inp = 
 
-        run_detector(inp, calib)
 
     # Runs the detector, taking in the input image and calibrator object. This means that it'll actually
     # detect the power port and then it'll calibrate to the power port green color. This method returns 
     # the contours of the actual power port.
 
-    def run_detector(self, inp, calibrator):
+    def run_detector(self, inp):
         img = input_feed;
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(hsv, calibrator.LOW_GREEN, calibrator.HIGH_GREEN)
+        mask = cv2.inRange(hsv, self.calibrator.LOW_GREEN, self.calibrator.HIGH_GREEN)
 		_, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 		contours.sort(key=lambda c: cv2.contourArea(c), reverse=True)
 
 		greens = hsv[np.where((mask == 255))]
 
-        calibrator.get_new_hsv(greens)
+        self.calibrator.get_new_hsv(greens)
 
         return contours
