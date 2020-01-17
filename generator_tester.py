6d6a2ef836c1400357d3637904d17e89b59d17ad
Amogh Shivaram <30681603+amoghs1912@users.noreply.github.com>
2020-01-16 20:34:56 -0800

    created VideoLiveGenerator and updated tester
    Created the VideoLiveGenerator class and incorporated it into the tester. Fixed DepthLiveGenerator to return the image array in RGB, not BGR.

--- generator_tester.py
+++ generator_tester.py
@@ -4,6 +4,7 @@ import time
 from depth_data_generator import DepthDataGenerator
 from depth_live_generator import DepthLiveGenerator
 from image_generator import ImageGenerator
+from video_live_generator import VideoLiveGenerator
 
 
 parser = argparse.ArgumentParser()
@@ -15,14 +16,22 @@ args = parser.parse_args()
 
 if args.generator == "DepthDataGenerator":
     depth_data_generator = DepthDataGenerator(args.input_depth, args.input_image)
-    _, arr = depth_data_generator.generate()
-    np.savetxt(args.destination, arr, delimiter=",", fmt="%s")
+    rgb, depth = depth_data_generator.generate()
+    np.savetxt(args.destination, depth, delimiter=",", fmt="%s")
+
 elif args.generator == "DepthLiveGenerator":
     depth_live_generator = DepthLiveGenerator(1)
     print("The tester will collect data after five seconds")
     time.sleep(5)
-    _, arr = depth_live_generator.generate()
-    np.savetxt(args.destination, arr, delimiter=",", fmt="%s")
+    rgb, depth = depth_live_generator.generate()
+    np.savetxt(args.destination, depth, delimiter=",", fmt="%s")
+
 elif args.generator == "ImageGenerator":
     image_generator = ImageGenerator(args.input_image)
-    image, _ = image_generator.generate()
+    rgb, depth = image_generator.generate()
+    print(rgb.shape)
+
+elif args.generator == "VideoLiveGenerator":
+    video_live_generator = VideoLiveGenerator(1)
+    rgb, depth = video_live_generator.generate()
+    print(rgb.shape)
