a60fee2fdaef5a9cd53493c7f95055e21dbe886e
Amogh Shivaram <30681603+amoghs1912@users.noreply.github.com>
2020-01-16 19:40:06 -0800

    revised tester class and updated generator code
    Made minor changes to DepthLiveGenerator and ImageGenerator; incorporated ImageGenerator into the tester.

--- image_generator.py
+++ image_generator.py
@@ -1,18 +1,10 @@
-import imghdr
 import cv2
-import numpy as np
 
-class imageGenerator:
 
-	# initilaze variables
-	def __init__(self, input):
-		self.input_path = input
-		frame = self.generate()
+class ImageGenerator:
+    def __init__(self, input_path):
+        self.input_path = input_path
 
-	def generate(self):
-		frame = None
-
-		if imghdr.what(self.input_path) is not None:
-			frame = cv2.imread(self.input_path)
-
-		return (cv2.cvtColor(frame, cv2.COLOR_BGR2RGB),None)
+    def generate(self):
+        frame = cv2.imread(self.input_path)
+        return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), None
