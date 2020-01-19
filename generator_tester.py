e49300f27d8176e2afe6549ebfa957f784108cd6
Amogh Shivaram <30681603+amoghs1912@users.noreply.github.com>
2020-01-19 14:04:50 -0800

    updated tester class

--- generator_tester.py
+++ generator_tester.py
@@ -1,6 +1,7 @@
 import argparse
 import numpy as np
 import time
+import cv2
 from depth_data_generator import DepthDataGenerator
 from depth_live_generator import DepthLiveGenerator
 from image_generator import ImageGenerator
@@ -14,6 +15,7 @@ parser.add_argument("-destination", nargs="?", help="path of the file to save th
 parser.add_argument("-depth", nargs="?", help="path of the CSV file to read")
 parser.add_argument("-image", nargs="?", help="path of the image file to read")
 parser.add_argument("-video", nargs="?", help="path of the video file to read")
+parser.add_argument("-play", action="store_true", help="specify whether or not to play live feeds or videos")
 args = parser.parse_args()
 
 if args.generator == "DepthDataGenerator":
@@ -23,10 +25,23 @@ if args.generator == "DepthDataGenerator":
 
 elif args.generator == "DepthLiveGenerator":
     depth_live_generator = DepthLiveGenerator(1)
-    print("The tester will collect data after five seconds")
-    time.sleep(5)
-    rgb, depth = depth_live_generator.generate()
-    np.savetxt(args.destination, depth, delimiter=",", fmt="%s")
+
+    if not args.play:
+        print("The tester will collect data after five seconds")
+        time.sleep(5)
+        rgb, depth = depth_live_generator.generate()
+        if args.destination is not None:
+            np.savetxt(args.destination, depth, delimiter=",", fmt="%s")
+    else:
+        print("Playing camera feed. Press \'q\' to quit")
+        while True:
+            rgb, depth = depth_live_generator.generate()
+            cv2.imshow("Video", cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR))
+            key = cv2.waitKey(1)
+            if key == ord('c') and args.destination is not None:
+                np.savetxt(args.destination, depth, delimiter=",", fmt="%s")
+            if key == ord('q'):
+                break
 
 elif args.generator == "ImageGenerator":
     image_generator = ImageGenerator(args.image)
@@ -38,7 +53,25 @@ elif args.generator == "VideoLiveGenerator":
     rgb, depth = video_live_generator.generate()
     print(rgb.shape)
 
+    if args.play:
+        print("Playing camera feed. Press \'q\' to quit")
+        while True:
+            rgb, _ = video_live_generator.generate()
+            cv2.imshow("Live video", cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR))
+            key = cv2.waitKey(1)
+            if key == ord('q'):
+                break
+
 elif args.generator == "VideoFileGenerator":
     video_file_generator = VideoFileGenerator(args.video)
     rgb, depth = video_file_generator.generate()
     print(rgb.shape)
+
+    if args.play:
+        print("Playing camera feed. Press \'q\' to quit")
+        while True:
+            rgb, _ = video_file_generator.generate()
+            cv2.imshow("Video", cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR))
+            key = cv2.waitKey(1)
+            if key == ord('q'):
+                break
