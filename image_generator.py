2fc0e8a5639ef914cec44584e98410bf70771093
Amogh Shivaram <30681603+amoghs1912@users.noreply.github.com>
2020-01-19 11:22:25 -0800

    added field of view method

--- image_generator.py
+++ image_generator.py
@@ -1,9 +1,15 @@
 import cv2
+import math
 
 
 class ImageGenerator:
     def __init__(self, input_path):
         self.input_path = input_path
+
+        self.FIELD_OF_VIEW = 70.42 * math.pi / 180.0
+
+    def get_field_of_view(self):
+        return self.FIELD_OF_VIEW
 
     def generate(self):
         frame = cv2.imread(self.input_path)
