48ed9da60ea890f21ebb3bf7212b7de383ba69f7
Michelle Pan <michelle-pan@users.noreply.github.com>
2020-01-23 20:21:57 -0800

    Combine loading bay and power port code
    Scripts now take in a second argument (either "lb" or "pp") that
    determines whether loading bay or power port object points are used.

--- sit-awareness/batch_test.py
+++ sit-awareness/batch_test.py
@@ -1,17 +1,22 @@
 import os
 import csv
 import cv2
-from cv_loading_bay import VisionTargetDetector
+import argparse
+from cv import PoseDetector
+
+parser = argparse.ArgumentParser()
+parser.add_argument("target", help="target to detect pose for", default="lb")
+args = parser.parse_args()
 
 with open('vision_output.csv', mode='w', newline='') as output_file:
 	output_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-	sorted_photos = sorted(os.listdir('test_photos_lb'))
+	sorted_photos = sorted(os.listdir('test_photos_' + args.target))
 	output_writer.writerow(["filename", "distance", "angle", "rx", "ry", "rz", "tx", "ty", "tz"])
 	#for every filename
 	for filename in sorted_photos:
-		vtd = VisionTargetDetector('test_photos_lb/' + filename)
+		pd = PoseDetector('test_photos_' + args.target + '/' + filename, args.target)
 		print(filename)
-		with vtd as v:
-			r,t = v.run_cv(display=False)
+		with pd as p:
+			r,t = p.get_values(display=False)
 			output_writer.writerow([filename, int(filename[filename.index('_')+1:-10]), int(filename[:filename.index('d')]),
 								   r[0], r[1], r[2], t[0], t[1], t[2]])
--- sit-awareness/test.py
+++ sit-awareness/test.py
@@ -2,27 +2,28 @@
 
 import argparse
 import cv2
-from cv_loading_bay import VisionTargetDetector
+from cv import PoseDetector
 
 # "python test.py 0" to run from camera in port 0
 # "python test.py video.mp4" to run from the video recording video.mp4
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input", help="read from the given camera or file", default="0")
+parser.add_argument("target", help="target to detect pose for", default="lb")
 args = parser.parse_args()
 
 print("reading from", args.input)
 
-vtd = VisionTargetDetector(args.input)
+pd = PoseDetector(args.input, args.target)
 wait_time = 0 # wait time of 0 will wait indefinitely for next key press
 
 # if input is from a camera, set wait time to 3 ms
 if len(args.input) == 1:
 	wait_time = 3
 
-with vtd as v:
+with pd as p:
 	while True:
-		v.run_cv()
+		p.get_values()
 		key = cv2.waitKey(wait_time)
 		if key == ord('q'):
 			break
