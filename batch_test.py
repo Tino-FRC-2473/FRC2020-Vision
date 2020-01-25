213071ef0fed981fec1cfb108d90a21c8b4b0c31
Michelle Pan <michelle-pan@users.noreply.github.com>
2020-01-25 12:14:37 -0800

    Make modifications to match PEP8 style

--- batch_test.py
+++ batch_test.py
@@ -12,14 +12,14 @@ parser.add_argument("target", help="target to detect pose for", choices=["loadin
 args = parser.parse_args()
 
 with open('vision_output.csv', mode='w', newline='') as output_file:
-	output_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-	sorted_photos = sorted(os.listdir(args.dir)
-	output_writer.writerow(["filename", "distance", "angle", "rx", "ry", "rz", "tx", "ty", "tz"])
+    output_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    sorted_photos = sorted(os.listdir(args.dir))
+    output_writer.writerow(["filename", "distance", "angle", "rx", "ry", "rz", "tx", "ty", "tz"])
 
-	for filename in sorted_photos:
-		print(filename)
-		with PoseDetector(args.dir + "/" + filename, args.target) as pd:
-			r,t = pd.get_values(display=False)
-			# example filename: 0degrees_18inches.png
-			output_writer.writerow([filename, int(filename[filename.index('_')+1:-10]), int(filename[:filename.index('d')]),
-								   r[0], r[1], r[2], t[0], t[1], t[2]])
+    for filename in sorted_photos:
+        print(filename)
+        with PoseDetector(args.dir + "/" + filename, args.target) as pd:
+            r, t = pd.get_values(display=False)
+            # example filename: 0degrees_18inches.png
+            output_writer.writerow([filename, int(filename[filename.index('_')+1:-10]), int(filename[:filename.index('d')]),
+                                   r[0], r[1], r[2], t[0], t[1], t[2]])
