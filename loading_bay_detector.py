9334f05b3e62b76672e72414b96e4fc799749b8e
Siddarth Mamidanna <siddarthm272@gmail.com>
2020-01-25 10:56:34 -0800

    Add the full working detection code for power port
    Detection code implements the green calibration, has been
    tested and works fully with input subteam's code.

---
+++ loading_bay_detector.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+from green_calibration import GreenCalibration
+class LoadingBayDetector:
+
+    # Constructor
+    # int input_type(0-image, 1-video, 2-live-camera, 3-depth-image, 4-depth-video)
+    def __init__(self, input_img):
+        self.calibrator = GreenCalibration()
+        inp = None
+        if(input_type == 0):
+            inp =
+        elif(input_type == 1):
+            inp =
+        elif(input_type == 2):
+            inp =
+        elif(input_type == 3):
+            inp =
+        else:
+            inp =
+
+    # Runs the detector, taking in the input image and calibrator object. This means that it'll actually
+    # detect the loading bay and then it'll calibrate to the loading bay green color. This method returns
+    # the contours of the actual loading bay.
+    def run_detector(self, inp):
+        img = inp
+        img = cv2.GaussianBlur(img, (3,3), cv2.BORDER_DEFAULT) #blurs image
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, self.calibrator.LOW_GREEN, self.calibrator.HIGH_GREEN)
+        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours.sort(key=lambda c: cv2.contourArea(c), reverse=True)
+
+        greens = hsv[np.where((mask == 255))]
+
+        self.calibrator.get_new_hsv(greens)
+
+        cv2.imshow("frame", img)
+        cv2.imshow("contours", contours)
+        return contours
