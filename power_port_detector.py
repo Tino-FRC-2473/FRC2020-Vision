0cb930d7d545b73f94d2a4904eab181a61f5acfb
Siddarth Mamidanna <siddarthm272@gmail.com>
2020-01-25 11:47:24 -0800

    Fix formatting issues

--- power_port_detector.py
+++ power_port_detector.py
@@ -1,4 +1,4 @@
-import cv2
+`import cv2
 from green_calibration import GreenCalibration
 
 
@@ -8,19 +8,27 @@ class PowerPortDetector:
         self.calibrator = GreenCalibration()
         self.input = generator
 
+
     # Runs the detector. This means that it'll actually detect the power port
     # and then it'll calibrate to the power port green color. This method returns
     # the contours of the actual power port.
 
     def run_detector(self):
         img = self.input.get_frame()
-        img = cv2.GaussianBlur(img, (3, 3), cv2.BORDER_DEFAULT)
+        img = cv2.GaussianBlur(img, (3,3), cv2.BORDER_DEFAULT) #blurs image
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, self.calibrator.LOW_GREEN, self.calibrator.HIGH_GREEN)
-        _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        contours.sort(key=lambda c: cv2.contourArea(c), reverse=True)
+		contours_return = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours = None
+        if len(contours_return == 3):
+            contours = contours_return[1]
+        else:
+            contours = contours_return[0]
 
-        greens = hsv[np.where((mask == 255))]
+
+		contours.sort(key=lambda c: cv2.contourArea(c), reverse=True)
+
+		greens = hsv[np.where((mask == 255))]
 
         self.calibrator.get_new_hsv(greens)
 
@@ -28,3 +36,4 @@ class PowerPortDetector:
 
     def get_generator():
         return self.input
+`
