4f2e0f9505fb3205084635e0c4a2696195f94381
Michelle Pan <michelle-pan@users.noreply.github.com>
2020-01-28 22:58:56 -0800

    Remove unnecessary imports

--- batch_test.py
+++ batch_test.py
@@ -3,11 +3,7 @@ import csv
 import cv2
 import argparse
 from pose_detector import PoseDetector
-# from depth_data_generator import DepthDataGenerator
-# from depth_live_generator import DepthLiveGenerator
 from image_generator import ImageGenerator
-from video_file_generator import VideoFileGenerator
-from video_live_generator import VideoLiveGenerator
 from loading_bay_detector import LoadingBayDetector
 from power_port_detector import PowerPortDetector
 
--- pose_detector.py
+++ pose_detector.py
@@ -7,11 +7,6 @@ import traceback
 import os
 from math import sin, cos
 from operator import add
-# from depth_data_generator import DepthDataGenerator
-# from depth_live_generator import DepthLiveGenerator
-from image_generator import ImageGenerator
-from video_file_generator import VideoFileGenerator
-from video_live_generator import VideoLiveGenerator
 from loading_bay_detector import LoadingBayDetector
 from power_port_detector import PowerPortDetector
 
