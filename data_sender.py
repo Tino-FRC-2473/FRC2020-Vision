91532355b338925949ccf529362d21095e5b74b0
Amogh Shivaram <30681603+amoghs1912@users.noreply.github.com>
2020-01-30 18:44:50 -0800

    updated data sender with correct format

--- data_sender.py
+++ data_sender.py
@@ -1,52 +1,31 @@
 import serial
 from pose_detector import PoseDetector
-from loading_bay_detector import LoadingBayDetector
-from power_port_detector import PowerPortDetector
 from video_live_generator import VideoLiveGenerator
+from power_port_detector import PowerPortDetector
 
 
 class DataSender:
     def __init__(self):
         name = "ttyTHS2"
         rate = 9600
+        port = 1
 
         self.s = serial.Serial("/dev/" + name, rate)
 
-        self.loading_bay_pd = PoseDetector(LoadingBayDetector(VideoLiveGenerator(1)))
-        self.power_port_pd = PoseDetector(PowerPortDetector(VideoLiveGenerator(1)))
+        self.pose_detector = PoseDetector(PowerPortDetector(VideoLiveGenerator(port)))
+
+    def convert_data(self):
+        rot, trans = self.pose_detector.get_values()
+
+        x = 0  # example:   296   cm      296
+        y = 0  # example:  1324   cm     1324
+        z = 0  # example: -  23.4 deg    -234
+
+        return x, y, z
 
     def send_data(self):
-        rot_l, trans_l = self.loading_bay_pd.get_values()
-        rot_p, trans_p = self.power_port_pd.get_values()
-
-        string = "S"
-
-        for num in rot_l:
-            string += format_data(num)
-
-        for num in trans_l:
-            string += format_data(num)
-
-        for num in rot_p:
-            string += format_data(num)
-
-        for num in trans_p:
-            string += format_data(num)
-
-        self.s.write(string + "E")
-
-
-def format_data(num):
-    num = int(num)
-
-    if num < -99:
-        return "-99"
-    if num > 999:
-        return "999"
-    if num < 0:
-        return "-" + ("%02d" % -num)
-    else:
-        return "%03d" % num
+        x, y, z = self.convert_data()
+        self.s.write("S %04d %04d %.1s%04d E\n" % (x, y, "+" if z >= 0 else "-", abs(z)))  # example: S 0296 1324 -0234 E
 
 
 data_sender = DataSender()
