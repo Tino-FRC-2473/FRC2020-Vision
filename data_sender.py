ee8e9c729ab103b9aa9280303a86d48fa51b0063
Rithu Eswaramoorthy <rithue2000@gmail.com>
2020-02-01 13:45:03 -0800

    Fix Python style check errors

--- data_sender.py
+++ data_sender.py
@@ -7,6 +7,7 @@ from video_live_generator import VideoLiveGenerator
 
 class DataSender:
     CAMERA_TILT = 9999
+
     def __init__(self):
         name = "ttyTHS2"
         rate = 9600
@@ -19,27 +20,24 @@ class DataSender:
     def convert_data(self):
         rot, trans = self.pose_calculator.get_values()
 
-        #converts camera coordinates (trans) into robot coordinates <robotx, roboty, robotz>
         robotx = trans[0]
         roboty = math.cos(self.CAMERA_TILT)*trans[1]-math.sin(self.CAMERA_TILT)*trans[2]
         robotz = math.sin(self.CAMERA_TILT)*trans[1]+math.sin(self.CAMERA_TILT)*trans[2]
 
-        #transformed camera coordinates (trans) to target coordinates
-        targety = math.cos(rot[2])trans[1] - math.sin(rot[2])(-math.sin(rot[1])trans[2]+math.cos(rot[1])trans[2]))
-        targetx = math.cos(rot[1])trans[0] + math.sin(rot[1])trans[2]
-        targetz =  math.sin(rot[2])trans[1] + math.cos(rot[2])(-math.sin(rot[1])trans[2]+math.cos(rot[1])trans[2]))
-
+        targety = math.cos(rot[2])trans[1]-math.sin(rot[2])(math.cos(rot[1])trans[2])-math.sin(rot[1])trans[2])
+        targetx = math.cos(rot[1])trans[0]+math.sin(rot[1])trans[2]
+        targetz =  math.sin(rot[2])trans[1] + math.cos(rot[2])(math.cos(rot[1])trans[2])-math.sin(rot[1])trans[2]))
         angle = math.degrees(math.acos(targety))
 
         if trans[0] is None:
             return 9999, 9999, 9999
 
-        return int(100 * robotx), int(100 * robotz), 10 * round(angle, 1)
+        return int(100*robotx), int(100*robotz), 10*round(angle, 1)
 
     def send_data(self):
         x, y, z = self.convert_data()
-        #self.s.write("S %04d %04d %.1s%04d E\n" % (x, y, "+" if z >= 0 else "-", abs(z)))
         self.s.write('S {:04d} {:04d} {:+04d} E\n'.format(x, y, z))
+
 
 data_sender = DataSender()
 
