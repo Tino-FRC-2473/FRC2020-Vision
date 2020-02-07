46639c788a3a0759d8be3bc1610ebfa7d1e5ee51
Michelle Pan <michellepan8@gmail.com>
2020-02-06 20:43:54 -0800

    Integrate capture code with Generator and Detector classes

--- capture_image.py
+++ capture_image.py
@@ -1,83 +1,60 @@
+import math
 import cv2
+import numpy as np
 import subprocess
 import imghdr
+import traceback
 import os
+from math import sin, cos
+from operator import add
 
-# finds angle between robot's heading and the perpendicular to the targets
-class ImageCapture:
+
+# finds rotation and translation of vision targets
+class CaptureImage:
 
     # initilaze variables
-    def __init__(self, input, angle, distance):
+    def __init__(self, detector):
 
-        self.input_path = input
-        self.angle = angle
-        self.distance = distance
+        self.detector = detector
+        self.generator = detector.get_generator()
 
-        try:
-            # if input is a camera port
-            self.input = cv2.VideoCapture(int(input))
-            self.set_camera_settings(input)
-        except:
-            # if input is a path
-            self.input = cv2.VideoCapture(input)
-            
-        frame = self.get_frame()
-        self.input.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-        self.input.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-        
+        frame, _ = self.generator.generate()
         self.SCREEN_HEIGHT, self.SCREEN_WIDTH = frame.shape[:2]
-        print(self.SCREEN_HEIGHT)
+        
+        # constant to scale down display windows
+        self.DISPLAY_CONSTANT = 0.8 if self.SCREEN_HEIGHT > 1000 else 1.0
+
 
     def __enter__(self):
         return self
 
     def __exit__(self, type, value, tb):
-        self.input.release()
         cv2.destroyAllWindows()
-        print("\nexited")
+        print("exited")
 
-    # sets exposure of the camera (will only work on Linux systems)
-    def set_camera_settings(self, camera_port):
+    def display_windows(self, frame, ref_frame):
+        cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
+        cv2.namedWindow("ref_frame", cv2.WINDOW_NORMAL)
 
-        camera_path = "/dev/video" + camera_port
+        cv2.resizeWindow("frame", (int(self.DISPLAY_CONSTANT * self.SCREEN_WIDTH), int(self.DISPLAY_CONSTANT * self.SCREEN_HEIGHT)))
+        cv2.resizeWindow("ref_frame", (int(self.DISPLAY_CONSTANT * self.SCREEN_WIDTH), int(self.DISPLAY_CONSTANT * self.SCREEN_HEIGHT)))
 
-        try:
-            subprocess.call(["v4l2-ctl", "-d", camera_path, "-c", "exposure_auto=1"])
-            subprocess.call(["v4l2-ctl", "-d", camera_path, "-c", "exposure_absolute=1"])
-        except:
-            print("exposure adjustment not completed")
+        cv2.moveWindow("frame", 350, 30)
+        cv2.moveWindow("ref_frame", 1000, 30)
 
-    # returns a frame corresponding to the input type
-    def get_frame(self):
-
-        frame = None
-
-        # if input is a camera port, use VideoCapture()
-        if self.input_path.isdigit():
-            _, frame = self.input.read()
-        # if input is an image, use cv2.imread()
-        elif imghdr.what(self.input_path) is not None:
-            frame = cv2.imread(self.input_path)
-        # if input is a video, use VideoCapture()
-        else:
-            _, frame = self.input.read()
-
-        return frame
+        cv2.imshow("frame", frame)
+        cv2.imshow("ref_frame", ref_frame)
 
     def update_frame(self):
-        frame = self.get_frame()
-        cv2.imshow("frame: " + str(self.input_path), frame)
+        frame, _ = self.generator.generate()
 
-        ref_frame = self.get_frame()
-        cv2.circle(frame, (320, 240), 2, (255,255,255), 2)
-        cv2.circle(frame, (320, 400), 2, (255,255,255), 2)
-        cv2.imshow("ref: " + str(self.input_path), frame)
+        ref_frame, _ = self.generator.generate()
+        cv2.circle(ref_frame, (int(self.SCREEN_WIDTH/2), int(self.SCREEN_HEIGHT/2 - 200)), 2, (255, 255, 255), 2)
+        cv2.circle(ref_frame, (int(self.SCREEN_WIDTH/2), int(self.SCREEN_HEIGHT/2 + 200)), 2, (255, 255, 255), 2)
 
-    def capture(self):
-        filename = "test_photos/" + str(self.angle) + "_degrees" + str(self.distance) + "inches.png"
-        cv2.imwrite(filename, self.get_frame())
+        self.display_windows(frame, ref_frame)
 
-    def prompt(self):
-        print("Capture another image! yay")
-        self.angle = input("Angle: ")
-        self.distance = input("Distance: ")
+    def capture(self, dir_name, angle, distance):
+        filename = dir_name + "/" + str(angle) + "degrees_" + str(distance) + "inches.png"
+        frame, _ = self.generator.generate()
+        cv2.imwrite(filename, frame)
--- run_capture.py
+++ run_capture.py
@@ -1,33 +1,39 @@
+#!/usr/bin/env python3
+import time
 import argparse
 import cv2
-from capture_image import ImageCapture
-
-# "python test.py 0" to run from camera in port 0
-# "python test.py video.mp4" to run from the video recording video.mp4
+import os
+from capture_image import CaptureImage
+from video_live_generator import VideoLiveGenerator
+from power_port_detector import PowerPortDetector
 
 parser = argparse.ArgumentParser()
-parser.add_argument("input", help="read from the given camera or file", default="0")
-parser.add_argument("angle", help="angle of capture", default="0")
-parser.add_argument("distance", help="distance of capture", default="60")
+parser.add_argument("dir_name", help="directory to save images to")
+parser.add_argument("port", help="camera port to read from")
 args = parser.parse_args()
 
-print("reading from", args.input)
-print("angle:", args.angle)
-print("distance:", args.distance)
+generator = VideoLiveGenerator(args.port)
+wait_time = 3
+target_detector = PowerPortDetector(generator)
 
-capture = ImageCapture(args.input, args.angle, args.distance)
-wait_time = 0 # wait time of 0 will wait indefinitely for next key press
+if not os.path.isdir(args.dir_name):
+    os.mkdir(args.dir_name)
 
-# if input is from a camera, set wait time to 3 ms
-if len(args.input) == 1:
-	wait_time = 3
+with CaptureImage(target_detector) as c:
 
-with capture as c:
-	while True:
-		c.update_frame()
-		key = cv2.waitKey(wait_time)
-		if key == ord('q'):
-			break
-		if key == ord('c'):
-			c.capture()
-			c.prompt()
+    angle = input("angle: ")
+    distance = input("distance: ")
+
+    while True:
+        c.update_frame()
+        key = cv2.waitKey(wait_time)
+
+        if key == ord('q'):
+            break
+        if key == ord('c'):
+            c.capture(args.dir_name, angle, distance)
+            if input("continue? (y/n) ") == "n":
+                break
+            
+            angle = input("angle: ")
+            distance = input("distance: ")
