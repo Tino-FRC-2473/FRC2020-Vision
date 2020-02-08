5e02221419eae63ba37317eb471826acbe1006b5
Amogh Shivaram <30681603+amoghs1912@users.noreply.github.com>
2020-02-08 10:37:02 -0800

    added updated math to DataSender

--- data_sender.py
+++ data_sender.py
@@ -24,17 +24,21 @@ class DataSender:
         if trans[0] is None:
             return 9999, 9999, 9999
 
-        robot_x = trans[0]
-        robot_y = math.cos(math.radians(self.CAMERA_TILT)) * trans[1] - math.sin(math.radians(self.CAMERA_TILT)) * trans[2]
-        robot_z = math.sin(math.radians(self.CAMERA_TILT)) * trans[1] + math.sin(math.radians(self.CAMERA_TILT)) * trans[2]
+        # robot_x = trans[0]
+        # robot_y = math.cos(math.radians(self.CAMERA_TILT)) * trans[1] - math.sin(math.radians(self.CAMERA_TILT)) * trans[2]
+        # robot_z = math.sin(math.radians(self.CAMERA_TILT)) * trans[1] + math.sin(math.radians(self.CAMERA_TILT)) * trans[2]
+        # 
+        # target_x = trans[0] * math.cos(math.radians(rot[1])) - trans[0] * math.sin(math.radians(rot[1]))
+        # target_y = trans[1] * math.cos(math.radians(rot[0])) + trans[1] * math.sin(math.radians(rot[0]))
+        # target_z = trans[2] * (math.sin(math.radians(rot[1])) + math.sin(math.radians(rot[1]))) * (math.sin(math.radians(rot[0])) + math.cos(math.radians(rot[0])))
+        #
+        # angle = math.degrees(math.acos(target_y))
 
-        target_x = trans[0] * math.cos(math.radians(rot[1])) - trans[0] * math.sin(math.radians(rot[1]))
-        target_y = trans[1] * math.cos(math.radians(rot[0])) + trans[1] * math.sin(math.radians(rot[0]))
-        target_z = trans[2] * (math.sin(math.radians(rot[1])) + math.sin(math.radians(rot[1]))) * (math.sin(math.radians(rot[0])) + math.cos(math.radians(rot[0])))
+        dx = trans[2] * math.cos(math.radians(self.CAMERA_TILT))
+        dy = trans[0]
+        angle = rot[1]
 
-        angle = math.degrees(math.acos(target_y))
-
-        return int(100 * robot_x), int(100 * robot_z), 10 * round(angle, 1)
+        return int(100 * dx), int(100 * dy), 10 * round(angle, 1)
 
     def send_data(self):
         x, y, angle = self.convert_data()
