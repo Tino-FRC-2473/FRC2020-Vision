Fix calculation for angle to use camera tilt

data_sender.py
@@ -1,5 +1,8 @@
 import math
 import serial
+import numpy as np
+import cv2
+from scipy.spatial.transform import Rotation as R
 from pose_calculator import PoseCalculator
 from power_port_detector import PowerPortDetector
 from video_live_generator import VideoLiveGenerator
@@ -26,7 +29,22 @@ class DataSender:
 
         dx = trans[2] * math.cos(math.radians(self.CAMERA_TILT))
         dy = trans[0]
-        angle = rot[1]
+
+        # calculate angle and compensate for camera tilt
+        camera_tilt_rad = math.radians(self.CAMERA_TILT)
+        rot_x = numpy.array([[1, 0, 0], [0, math.cos(math.radians(rot[0])), -math.sin(math.radians(rot[0]))], [0, math.sin(math.radians(rot[0])), math.cos(math.radians(rot[0]))]])
+        rot_y = numpy.array([[math.cos(math.radians(rot[1])), 0, math.sin(math.radians(rot[1]))], [0, 1, 0], [-math.sin(math.radians(rot[1])), 0, math.cos(math.radians(rot[1]))]])
+        rot_z = numpy.array([[math.cos(math.radians(rot[2])), math.sin(math.radians(rot[2])), 0], [-math.sin(math.radians(rot[2])), math.cos(math.radians(rot[2])), 0], [0, 0, 1]])
+
+        rot_camera = numpy.array([[math.cos(camera_tilt_rad), 0, math.sin(camera_tilt_rad)], [0, 1, 0], [-math.sin(camera_tilt_rad), 0, math.cos(camera_tilt_rad)]])
+
+        r_target = np.linalg.inv(rot_c) @ rot_z @ rot_y @ rot_x
+
+        r = R.from_matrix(r_target)
+        new_rotations = r.as_euler('zyx', degrees=True)
+
+        angle = new_rotations[1]
+
 
         return int(100 * dx), int(100 * dy), 10 * round(angle, 1)
 
@@ -39,3 +57,8 @@ data_sender = DataSender()
 
 while True:
     data_sender.send_data()
+
+    # show target visualization
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break