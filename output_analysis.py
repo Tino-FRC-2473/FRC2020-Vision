5c3ffa883c3f3589f2cdb234470dc61c4496467a
Michelle Pan <michelle-pan@users.noreply.github.com>
2020-02-15 12:13:19 -0800

    Change focal length and switch to SolvePnPRansac
    Change focal length to approximately 600, as observed from camera
    calibration output using chessboard images

--- output_analysis.py
+++ output_analysis.py
@@ -14,17 +14,18 @@ dist_constants = []
 with open(args.input, newline='') as input_file:
     reader = csv.DictReader(input_file)
     for row in reader:
-        angle, dist = float(row["angle"]), float(row["distance"])
+        angle, dist = float(row["angle"]), float(math.cos(math.radians(CAMERA_ANGLE)) * row["distance"])
         ry, tz = float(row["ry"]), float(row["tz"])
-    if angle == 0:
-        angle_errors.append(abs(ry - angle))
-    else:
-        angle_errors.append(abs((ry - angle)) / angle)
-    dist_errors.append(abs((tz - dist)) / dist)
-    dist_constants.append(dist / tz)
+        if angle == 0:
+            angle_errors.append(abs(ry - angle))
+        else:
+            angle_errors.append(abs((ry - angle)) / angle)
+        dist_errors.append(abs((tz - dist)) / dist)
+        dist_constants.append(dist / tz)
 
 print("average % angle error: ", round(100 * np.mean(angle_errors), 3))
 print("average % distance error: ", round(100 * np.mean(dist_errors), 3))
 
 if args.distance_constant:
     print("average distance constant: ", round(np.mean(dist_constants), 7))
+    print("distance constant variance:", round(variance(dist_constants), 3))
