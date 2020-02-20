600876a510f02bf90cd09fda3c7d435eb3f4c7b9
Amogh Shivaram <30681603+amoghs1912@users.noreply.github.com>
2020-02-19 19:46:55 -0800

    Clean up calibration code

--- green_calibration.py
+++ green_calibration.py
@@ -1,21 +1,16 @@
-import math
 import cv2
 import numpy as np
-import subprocess
-import imghdr
 import random
 import matplotlib.pyplot as plt
 
 
 class GreenCalibration:
-
     # This is how many standard deviations below and above the mean the low green/high green will be
     H_STD_TOLERANCE = 3.25
     S_STD_TOLERANCE = 3.5
     V_STD_TOLERANCE = 2
 
     def __init__(self, csv_data='green_data.csv'):
-
         rgb_data = np.loadtxt(csv_data, dtype=np.uint8, delimiter=',')
         bgr_data = np.copy(rgb_data)
         bgr_data[:, 0] = rgb_data[:, 2]
@@ -28,34 +23,40 @@ class GreenCalibration:
         self.low_green = np.array([68, 100, 50])
         self.high_green = np.array([84, 255, 255])
 
-    def get_new_hsv(self, greens, img, contours):
-        if (len(greens) == 0 or len(contours) == 0):
+    def get_new_hsv(self, hsv, contours):
+        if len(contours) == 0:
             return
-            
-        mask = np.zeros((480,640,1), dtype=np.uint8)
-        cnt = contours[0]
-        cv2.drawContours(mask, [cnt], 0, (255,255,255), thickness=cv2.FILLED)
-        # print(mask.shape)
-        cv2.imshow("mask", mask)
-        my_greens = img[np.where((mask[:,:,0] == 255))]
-        print(my_greens)
-        if(len(my_greens) == 0):
+
+        # Creates empty mask with shape (480, 640, 1)
+        mask = np.zeros((480, 640, 1), dtype=np.uint8)
+
+        # Populates empty mask with the contour of the target
+        cv2.drawContours(mask, [contours[0]], 0, (255, 255, 255), thickness=cv2.FILLED)
+
+        # List of all HSV values inside the contour
+        greens = hsv[np.where((mask[:, :, 0] == 255))]
+
+        if len(greens) == 0:
             return
+
         for i in range(100):
-            row = random.randrange(0, len(my_greens))
-            print(my_greens[row])
-            self.true_green_vals = np.append(self.true_green_vals, np.reshape(np.array(my_greens[row]), (1, 1, 3)), 0)
+            row = random.randrange(0, len(greens))
+            self.true_green_vals = np.append(self.true_green_vals, np.reshape(np.array(greens[row]), (1, 1, 3)), 0)
 
         h = self.true_green_vals[:, :, 0]
         s = self.true_green_vals[:, :, 1]
         v = self.true_green_vals[:, :, 2]
-        # self.show_histogram(h,s,v)
-        low_h, low_s, low_v = (h.mean() - self.H_STD_TOLERANCE * h.std()), (s.mean() - self.S_STD_TOLERANCE * s.std()), (v.mean() - self.V_STD_TOLERANCE * v.std())
-        high_h, high_s, high_v = (h.mean() + self.H_STD_TOLERANCE * h.std()), (s.mean() + self.S_STD_TOLERANCE * s.std()), (v.mean() + self.V_STD_TOLERANCE * v.std())
+
+        low_h, low_s, low_v = (h.mean() - self.H_STD_TOLERANCE * h.std()),\
+                              (s.mean() - self.S_STD_TOLERANCE * s.std()),\
+                              (v.mean() - self.V_STD_TOLERANCE * v.std())
+
+        high_h, high_s, high_v = (h.mean() + self.H_STD_TOLERANCE * h.std()),\
+                                 (s.mean() + self.S_STD_TOLERANCE * s.std()),\
+                                 (v.mean() + self.V_STD_TOLERANCE * v.std())
         
         self.low_green = np.array([int(low_h), int(low_s), int(low_v)])
         self.high_green = np.array([int(high_h), int(high_s), int(high_v)])
-        print(self.low_green, self.high_green)
 
     def show_histogram(self, h, s, v):
         plt.figure(figsize=(12, 4))
@@ -72,8 +73,8 @@ class GreenCalibration:
         plt.show()
         plt.close()
 
-    def get_low_green():
+    def get_low_green(self):
         return self.low_green
 
-    def get_high_green():
+    def get_high_green(self):
         return self.high_green
--- loading_bay_detector.py
+++ loading_bay_detector.py
@@ -1,18 +1,12 @@
 import cv2
-import numpy as np
 from green_calibration import GreenCalibration
 
 
 class LoadingBayDetector:
 
-    # Constructor
     def __init__(self, generator):
         self.calibrator = GreenCalibration()
         self.input = generator
-
-    # Runs the detector. This means that it'll actually detect the loading bay
-    # and then it'll calibrate to the loading bay green color. This method returns
-    # the contours of the actual loading bay.
 
     def run_detector(self):
         img, _ = self.input.generate()
@@ -22,20 +16,13 @@ class LoadingBayDetector:
 
         img = cv2.GaussianBlur(img, (3, 3), cv2.BORDER_DEFAULT)
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        hsv[...,1] = hsv[:,:,1]*3
+
         mask = cv2.inRange(hsv, self.calibrator.low_green, self.calibrator.high_green)
         contours_return = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        contours = None
-        if len(contours_return) == 3:
-            contours = contours_return[1]
-        else:
-            contours = contours_return[0]
+        contours = contours_return[len(contours_return) == 3]
         contours.sort(key=lambda c: cv2.contourArea(c), reverse=True)
 
-        greens = hsv[np.where((mask == 255))]
-
-        self.calibrator.get_new_hsv(greens)
-
+        self.calibrator.get_new_hsv(hsv, contours)
         return contours, mask
 
     def get_generator(self):
