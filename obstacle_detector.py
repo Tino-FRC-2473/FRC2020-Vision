58092de938e77d990cea9e60c7eb80923c7abb3e
pguduguntla <praneethg347@gmail.com>
2020-02-19 20:22:38 -0800

    Create skeleton of class ObstacleDetector

---
+++ obstacle_detector.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+
+class ObstacleDetector:
+
+    # Constructor
+    def __init__(self, generator):
+
+       
+    def run_detector(self):
+        img, _ = self.input.generate()
+
+        if img is None:
+            return None, None
+
+        return contours, mask
+
+    def get_generator(self):
+        return self.input
