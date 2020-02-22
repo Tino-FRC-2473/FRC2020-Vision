a484d6c3b9d89a6d7f491adda5c70f85c3b1e986
Amogh Shivaram <30681603+amoghs1912@users.noreply.github.com>
2020-02-22 12:03:53 -0800

    Make ball detection code more concise

--- power_cell_detector.py
+++ power_cell_detector.py
@@ -4,8 +4,6 @@ import numpy as np
 
 
 class PowerCellDetector:
-
-    # Constructor
     def __init__(self, generator):
         self.input = generator
         self.LOW_YELLOW = np.array([17, 120, 50])
@@ -31,26 +29,18 @@ class PowerCellDetector:
 
         # Dilation increases the overall area of the object to accentuate the contours.
         mask = cv2.dilate(mask, None, iterations=10)
-
-        # Making a copy to make sure that the original mask is preserved when preparing for the adaptiveThreshold
-        mask_copy = mask.copy()
-        mask_copy[mask_copy > 1] = 1
-        output = np.multiply(mask_copy, hsv[:, :, 1])
+        output = (mask == 255) * hsv[:, :, 1]
 
         # The second-to-last parameter is the block size, which is the width of the square region to use to calculate
         # the threshold for a pixel--the threshold is different in different regions of the frame.
-        th2 = cv2.adaptiveThreshold(output, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 15, 10)
-
-        circles = cv2.HoughCircles(th2, cv2.HOUGH_GRADIENT, 1.8, 40, param1=70, param2=50, minRadius=20, maxRadius=60)
+        th = cv2.adaptiveThreshold(output, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 15, 10)
+        circles = cv2.HoughCircles(th, cv2.HOUGH_GRADIENT, 1.8, 40, param1=70, param2=50, minRadius=20, maxRadius=60)
         if circles is None:
-            return None, mask, img, depth_frame
+            return None, depth_frame
 
         detected_circles = np.uint16(np.around(circles))
-        circles = []
-        for x, y, r in detected_circles[0, :]:
-            circles.append([x, y, r])
-
-        return circles, mask, img, depth_frame
+        circles = [[x, y, r] for x, y, r in detected_circles[0, :]]
+        return circles, depth_frame
 
     def get_generator(self):
         return self.input
