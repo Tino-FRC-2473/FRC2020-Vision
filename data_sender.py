cd477cf833c7cc4d08b5f19da1c4a61a1f1e8f8a
Amogh Shivaram <30681603+amoghs1912@users.noreply.github.com>
2020-02-25 22:58:23 -0800

    Change structure of DataSender class and make ball data optional

--- data_sender.py
+++ data_sender.py
@@ -9,10 +9,11 @@ from video_live_generator import VideoLiveGenerator
 
 
 class DataSender:
-    def __init__(self, name="ttyS0", rate=9600, video_port=0, depth_port=1):
+    def __init__(self, name="ttyS0", rate=9600, video_port=0, depth_port=1, run_depth=True):
         self.s = serial.Serial("/dev/" + name, rate)
         self.pose_calculator = PoseCalculator(PowerPortDetector(VideoLiveGenerator(video_port)))
         self.ball_finder = BallFinder(PowerCellDetector(DepthLiveGenerator(depth_port)))
+        self.run_depth = run_depth
 
     def get_power_port_data(self):
         rot, trans = self.pose_calculator.get_values()
@@ -24,7 +25,7 @@ class DataSender:
         dx = trans[2]
         angle = -rot[1]
 
-        return int(100 * dx), int(100 * dy), int(10 * round(angle, 1))
+        return "{:+05d} {:+05d} {:+05d}".format(int(100 * dx), int(100 * dy), int(10 * round(angle, 1)))
 
     def get_ball_detection_data(self):
         ball_data, obstacle_present = self.ball_finder.get_balls()
@@ -33,15 +34,16 @@ class DataSender:
         for ball in ball_data:
             formatted.append([9999, 9999] if not ball else [int(100 * ball[0]), int(10 * round(ball[1], 1))])
 
-        return formatted, obstacle_present
+        data_string = "".join(" {:+05d} {:+05d}".format(ball[0], ball[1]) for ball in formatted)
+        data_string += " " + str(int(obstacle_present))
+
+        return data_string
 
     def send_data(self):
-        x, y, angle = self.get_power_port_data()
-        data_to_send = "{:+05d} {:+05d} {:+05d} ".format(x, y, angle)
+        data_to_send = self.get_power_port_data()
 
-        balls, obstacle_present = self.get_ball_detection_data()
-        data_to_send += " ".join("{:+05d} {:+05d}".format(ball[0], ball[1]) for ball in balls)
-        data_to_send += " " + str(int(obstacle_present))
+        if self.run_depth:
+            data_to_send += self.get_ball_detection_data()
 
         self.s.write(bytes("S " + data_to_send + " E", "utf-8"))
 
