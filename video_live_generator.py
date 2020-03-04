cd8c27c1fabeac8f764effdf13dd56cb17b45a07
Amogh Shivaram <30681603+amoghs1912@users.noreply.github.com>
2020-03-03 18:45:31 -0800

    Add lines to allow running data_sender.py with sudo

--- video_live_generator.py
+++ video_live_generator.py
@@ -38,7 +38,7 @@ class VideoLiveGenerator:
 
     def get_frame(self):
         _, frame = self.input.read()
-        frame = np.rot90(frame, 3).copy()
+        frame = np.rot90(frame, 1).copy()
         return frame
 
     def get_horizontal_fov(self):
