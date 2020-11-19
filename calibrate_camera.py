65a5bc5db88f1728ecedbf4908578d91b538b602
AG6GR <slhe@alumni.princeton.edu>
2020-11-18 20:47:25 -0800

    Add images taken during meeting with 1.25" checkerboard

--- calibrate_camera.py
+++ calibrate_camera.py
@@ -11,6 +11,7 @@ args = parser.parse_args()
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
+# Squares are 1.25" across
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((6 * 7, 3), np.float32)
 objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
@@ -33,8 +34,8 @@ for image in os.listdir(args.dir):
         imgpoints.append(corners)
         # Draw and display the cornerss
         cv2.drawChessboardCorners(img, (7, 6), corners2, ret)
-        # cv2.imshow('img', img)
-        cv2.waitKey(200)
+        cv2.imshow('img', img)
+        cv2.waitKey()
         ret, mtx, dist, rvec, tvec = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
         print("matrix:", mtx)
